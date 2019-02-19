f45f39a17368f35cf9ae8afe1c614daa295b2fb3
James Smoot <jimi@jsfour.com>
2019-02-19 08:40:21 -0800

    better url

--- home-monitor.go
+++ home-monitor.go
@@ -11,13 +11,13 @@ import (
 const PushEndpoint = "https://push.statuscake.com/"
 
 func sendStatus() error {
-	log.Println("Sending status")
 
 	pk := os.Getenv("STATUS_CAKE_PK")
 	test_id := os.Getenv("STATUS_CAKE_TEST_ID")
 
 	t0 := time.Now().Unix()
 	url := PushEndpoint + fmt.Sprintf("?PK=%v&TestID=%v&time=%v", pk, test_id, t0)
+	log.Println("Sending status", url)
 	_, err := http.Get(url)
 	if err != nil {
 		log.Println("Send status failed")
