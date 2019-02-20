61e6dbf76de4e4018dfcc85b22bb519be88194e2
James Smoot <jimi@jsfour.com>
2019-02-19 20:45:52 -0800

    checking resp

--- home-monitor.go
+++ home-monitor.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
@@ -28,13 +29,19 @@ func sendStatus() error {
 	pk := os.Getenv("STATUS_CAKE_PK")
 	test_id := os.Getenv("STATUS_CAKE_TEST_ID")
 
-	t0 := time.Now().Unix()
+	t0 := 0
 	url := PushEndpoint + fmt.Sprintf("?PK=%v&TestID=%v&time=%v", pk, test_id, t0)
 	log.Println("Sending status", url)
-	_, err := http.Get(url)
+	res, err := http.Get(url)
 	if err != nil {
 		log.Println("Send status failed")
 		return err
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(res.Body)
+	s := buf.String()
+	if s != "success" {
+		log.Fatalln(s)
 	}
 	return err
 }
