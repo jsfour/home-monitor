b326f5bf506595b0d2ae52eb9c28940629f8604f
James Smoot <jimi@jsfour.com>
2019-03-27 21:36:58 -0700

    update ip

--- home-monitor.go
+++ home-monitor.go
@@ -6,6 +6,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/route53"
 )
 
 const PublicIpEndpoint = "https://api.ipify.org"
@@ -22,6 +26,87 @@ func getPublicIp() (string, error) {
 	s := buf.String()
 
 	return s, nil
+}
+
+func sendDns() error {
+	log.Println("Updating dns")
+	sess := session.Must(session.NewSession(&aws.Config{}))
+
+	svc := route53.New(sess)
+
+	ip, err := getPublicIp()
+
+	if err != nil {
+		return err
+	}
+
+	dnsName := os.Getenv("DNS_HOSTNAME")
+	zoneID := os.Getenv("ROUTE53_ZONE_ID")
+	var ttl int64 = 300
+
+	recordSetQuery := &route53.ListResourceRecordSetsInput{
+		HostedZoneId:    aws.String(zoneID),
+		StartRecordName: aws.String(dnsName),
+	}
+
+	res, err := svc.ListResourceRecordSets(recordSetQuery)
+	if err != nil {
+		return err
+	}
+
+	var changes []*route53.Change
+	for _, recSet := range res.ResourceRecordSets {
+		if *recSet.Name != dnsName {
+			continue
+		}
+		for _, record := range recSet.ResourceRecords {
+			log.Println(*record.Value)
+			if *record.Value == ip {
+				log.Println("No change in IP")
+				return nil
+			}
+			// need to change
+
+			ttl = *recSet.TTL
+			chg := &route53.Change{
+				Action:            aws.String("DELETE"),
+				ResourceRecordSet: recSet,
+			}
+			changes = append(changes, chg)
+
+			break
+		}
+	}
+
+	newChange := &route53.Change{
+		Action: aws.String("CREATE"),
+		ResourceRecordSet: &route53.ResourceRecordSet{
+			Name: aws.String(dnsName),
+			Type: aws.String("A"),
+			TTL:  aws.Int64(ttl),
+			ResourceRecords: []*route53.ResourceRecord{
+				{
+					Value: aws.String(ip),
+				},
+			},
+		},
+	}
+
+	changes = append(changes, newChange)
+
+	changeSet := &route53.ChangeResourceRecordSetsInput{
+		ChangeBatch: &route53.ChangeBatch{
+			Changes: changes,
+		},
+		HostedZoneId: aws.String(zoneID),
+	}
+
+	_, err = svc.ChangeResourceRecordSets(changeSet)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func sendStatus() error {
@@ -48,6 +133,10 @@ func sendStatus() error {
 
 func main() {
 	err := sendStatus()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = sendDns()
 	if err != nil {
 		log.Fatalln(err)
 	}
--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@ Required env vars:
 
 STATUS_CAKE_PK
 STATUS_CAKE_TEST_ID
+AWS_ACCESS_KEY
+AWS_SECRET_KEY
+DNS_HOSTNAME // hostname to keep up to date
+ROUTE53_ZONE_ID // zoneid to keep up to date
